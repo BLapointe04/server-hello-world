b09a54e572b0281195690efb2f7170f3750b3692
Brayden Lapointe <lapointb@email.sc.edu>
2025-10-16 22:44:12 -0400

    Initial commit: server hello world

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_req, res) => {
+  res.send("Hello World from Server Side!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
